d86d4fedf1bf71c333264f0f07f5f4ddbef5ab3c
Dan Dolney <ddolney@uft-int.com>
2021-02-10 11:46:25 -0600

    Added another table.

---
+++ Baseline_Testing/Migrations/1.0.0-Baseline/003_20210210-1145_adaddolney.sql
@@ -0,0 +1,22 @@
+﻿-- <Migration ID="7f68efc9-bb71-425a-b1cb-10281ff2f5c9" />
+GO
+
+PRINT N'Creating [dbo].[NOT_LostSeaLimbo]'
+GO
+CREATE TABLE [dbo].[NOT_LostSeaLimbo]
+(
+[id] [int] NOT NULL IDENTITY(1, 1),
+[CustomerName] [varchar] (250) NULL,
+[ServerName] [varchar] (200) NULL,
+[dbname] [varchar] (250) NULL,
+[Status] [varchar] (25) NULL,
+[Transaction_id] [int] NULL,
+[OperatorActionDate] [datetime] NULL,
+[TransactionStatus_Id] [smallint] NULL,
+[Activity] [varchar] (200) NULL,
+[LastActivityMessage] [varchar] (100) NULL,
+[CreatedDate] [datetime] NOT NULL,
+[IsReported] [bit] NOT NULL,
+[UnresolvedResendDate] [datetime] NULL
+)
+GO
